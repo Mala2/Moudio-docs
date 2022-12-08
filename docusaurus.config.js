7574ae0122f3655879b2e1ec9a84e0c380962bff
Mo <mfm1500@hotmail.com>
2022-12-08 09:13:01 +0000

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
             },
             {
               label: 'Hardware',
-              to: 'docs/hardware',
+              to: 'docs/Schematics',
             },
             {
               label: "GitHub",
